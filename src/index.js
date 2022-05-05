c89a7b52eb927dc8468c597db9382a01062634d0
Viraj Shah <virajshah1104@gmail.com>
2022-05-05 15:33:18 -0700

    Added Authentication

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import reportWebVitals from './reportWebVitals';
 
+Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
